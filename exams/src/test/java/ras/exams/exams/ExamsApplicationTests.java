7c4c7bf51dcd678ccc1f77ceabc104fe42640962
Gonçalo <smgonsa@hotmail.com>
2023-12-19 14:13:32 +0000

    first commit

---
+++ exams/src/test/java/ras/exams/exams/ExamsApplicationTests.java
@@ -0,0 +1,13 @@
+package ras.exams.exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
